c15fe986896e5c8e7aad2b94d41bb29151283036
Ap0c <Ap0c@users.noreply.github.com>
2015-02-27 16:29:42 +0000

    Filled out File methods. Added class variable for storing data file directory.

--- src/db/Files.java
+++ src/db/Files.java
@@ -14,23 +14,49 @@ import java.io.IOException;
 
 class File {
 
+	// ----- Class Variables ----- //
+
+	private static final String dataDir = "data/";
+
 	// ----- Instance Methods ----- //
 
 	/**
 	 * Saves a table to a file, serializing the object data.
 	 * 
 	 * @param table the table object to be saved.
+	 * @param name name of the table, used to define filename.
 	 * @since 0.3
 	 */
-	public void saveTable (Table table) throws IOException {}
+	public void saveTable (Table table, String name) throws IOException {
+
+		FileOutputStream file = new FileOutputStream(dataDir + name);
+		ObjectOutputStream objectOut = new ObjectOutputStream(file);
+
+		objectOut.write(table);
+		objectOut.close();
+		file.close();
+
+	}
 
 	/**
 	 * Reads a table from file, file must be a serialized Table object.
 	 * 
 	 * @param tableName the name of the file containing the Table object.
+	 * @return the Table object that has been read in.
 	 * @since 0.3
 	 */
-	public void readTable (String tableName)
-		throws IOException, ClassNotFoundException {}
+	public Table readTable (String tableName)
+		throws IOException, ClassNotFoundException {
+
+		FileInputStream file = new FileInputStream(dataDir + tableName);
+		ObjectInputStream objectIn = new ObjectInputStream(file);
+
+		Table table = (Table) objectIn.readObject();
+		objectIn.close();
+		file.close();
+
+		return table;
+
+	}
 	
 }
